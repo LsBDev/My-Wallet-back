79e21f90053dcc32d01cac289251502dfe6e4525
LsB.Dev <prof.luifel@gmail.com>
2023-07-30 15:03:11 -0300

    implementação rota de transações

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -7,7 +7,7 @@ import { transactionSchema } from "../schemas/transaction.schemas.js";
 const transactionRouter = Router()
 transactionRouter.use(authValidation)
 
-transactionRouter.post("transactions", validateSchema(transactionSchema), createTransaction)
-transactionRouter.get("transactions", getTransactions)
+transactionRouter.post("/transactions", validateSchema(transactionSchema), createTransaction)
+transactionRouter.get("/transactions", getTransactions)
 
 export default transactionRouter
